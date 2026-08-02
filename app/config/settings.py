3fed1c7373dc53eda108ab7a961962f232d054e6
root <root@pi-prototype.home.macl.at>
2026-08-02 20:44:45 +0200

    fix: move UserLanguageMiddleware after AuthenticationMiddleware

--- app/config/settings.py
+++ app/config/settings.py
@@ -32,10 +32,10 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
-    "workbench.middleware.UserLanguageMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "workbench.middleware.UserLanguageMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]

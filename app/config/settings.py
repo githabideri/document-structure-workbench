0487106960715774c22b5a61f87034a8c6816e08
root <root@pi-prototype.home.macl.at>
2026-08-02 19:32:20 +0200

    Remove missing LanguagePreferenceMiddleware from MIDDLEWARE

--- app/config/settings.py
+++ app/config/settings.py
@@ -36,7 +36,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "workbench.middleware.LanguagePreferenceMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"

08bdd436ce3fafcfd466f7d46e2bcf6e095c354f
root <root@pi-prototype.home.macl.at>
2026-08-02 19:36:12 +0200

    Support DSW_DATABASE_PATH env var for database location

--- app/config/settings.py
+++ app/config/settings.py
@@ -64,7 +64,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": Path(os.environ.get(
+            "DSW_DATABASE_PATH", BASE_DIR / "db.sqlite3"
+        )),
     }
 }
 
